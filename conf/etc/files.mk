a8dd0f825626f8d29eaae316a163a27fa0d8694e
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2011-04-10 11:24:42 -0400

    Port of mkdosfs from dosfstools 3.0.11

--- conf/etc/files.mk
+++ conf/etc/files.mk
@@ -40,6 +40,10 @@ ifeq ($(CONFIG_CMD_MOUNT),y)
 FILES+= 	$(SRCDIR)/usr/sbin/mount/mount
 endif
 
+ifeq ($(CONFIG_CMD_MKDOSFS),y)
+FILES+= 	$(SRCDIR)/usr/sbin/mkdosfs/mkdosfs
+endif
+
 ifneq ($(_QUICK_),1)
 ifneq ($(CONFIG_TINY),y)
 FILES+=		$(SRCDIR)/usr/sample/hello/hello
