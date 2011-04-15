76db52f4fafedd29128bdc1d8e00137b321e49ac
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2011-04-15 16:56:09 -0400

    Print out boot sector during partition scan.

--- bsp/drv/dev/block/hdd.c
+++ bsp/drv/dev/block/hdd.c
@@ -604,6 +604,21 @@ static int read_during_setup(struct ata_disk *disk, uint64_t lba, uint8_t *buf, 
   return 0;
 }
 
+static void dump1(uint8_t *buf, size_t count) {
+  size_t off = 0;
+  while (count) {
+    int linelen = count > 32 ? 32 : count;
+    printf("%03d ", off);
+    while (linelen) {
+      printf("%02x", buf[off]);
+      linelen--;
+      count--;
+      off++;
+    }
+    printf("\n");
+  }
+}
+
 /* Read a disk's partition table. */
 static void setup_partitions(struct driver *self, struct ata_disk *disk) {
   uint8_t *sector0 = kmem_alloc(SECTOR_SIZE);
@@ -612,6 +627,9 @@ static void setup_partitions(struct driver *self, struct ata_disk *disk) {
     kmem_free(sector0);
     return;
   }
+
+  printf("Disk %s's root sector:\n", disk->devname);
+  dump1(sector0, SECTOR_SIZE);
 
   if (0xaa55 == (* (uint16_t *) (&sector0[SECTOR_SIZE - 2]))) {
     int partition;
@@ -679,21 +697,6 @@ static void fixup_string_endianness(uint8_t *p, size_t size) {
     size -= 2;
   }
 }
-
-/* static void dump1(uint8_t *buf, size_t count) { */
-/*   size_t off = 0; */
-/*   while (count) { */
-/*     int linelen = count > 32 ? 32 : count; */
-/*     printf("%03d ", off); */
-/*     while (linelen) { */
-/*       printf("%02x", buf[off]); */
-/*       linelen--; */
-/*       count--; */
-/*       off++; */
-/*     } */
-/*     printf("\n"); */
-/*   } */
-/* } */
 
 static int setup_disk(struct driver *self, struct ata_controller *c, int disknum) {
   struct ata_disk *disk = kmem_alloc(sizeof(struct ata_disk));
